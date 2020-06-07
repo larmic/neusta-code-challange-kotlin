295c8df2baa0e7d81081c9ef77d0edc8f5d85d77
Lars Michaelis <mail@larmic.de>
2020-06-07 20:22:05 +0200

    a bit more clean code

--- src/main/kotlin/de/neusta/ncc/domain/RoomRepository.kt
+++ src/main/kotlin/de/neusta/ncc/domain/RoomRepository.kt
@@ -6,12 +6,7 @@ interface RoomRepository {
      * Replaced already existing rooms by new collection of rooms
      */
     fun replaceRooms(rooms: List<Room>)
-
-    /**
-     * @return [Room] matching exactly given room number.
-     */
     fun findByRoomNumber(roomNumber: String): Room?
-
     fun getRooms(): List<Room>
 
 }
