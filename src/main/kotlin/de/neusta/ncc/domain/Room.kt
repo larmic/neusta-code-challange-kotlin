4b95e8473a128a3f52a4e4a5d3448d092593d15b
Lars Michaelis <mail@larmic.de>
2020-06-07 20:07:32 +0200

    a bit more clean code

--- src/main/kotlin/de/neusta/ncc/domain/Room.kt
+++ src/main/kotlin/de/neusta/ncc/domain/Room.kt
@@ -3,7 +3,8 @@ package de.neusta.ncc.domain
 class Room(val roomNumber: String, val persons: List<Person> = emptyList()) {
 
     init {
-        assert(roomNumber.length == 4 && roomNumber.isNotBlank()) { "Room with number $roomNumber must have 4 arbitrary characters." }
+        assert(roomNumber.validateRoomNumber()) { "Room with number $roomNumber must have 4 arbitrary characters." }
     }
 
+    private fun String.validateRoomNumber() = this.length == 4 && this.isNotBlank()
 }
